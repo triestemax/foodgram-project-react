4cbdfb8c5f3d4673a81875ca05da601845f92613
triestemax <shabanov_maxim@mail.ru>
2023-10-13 12:44:44 +0300

    permission в settings изменен

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
